7bf1432cbebf4a45c04c4efff23943f3d262505d
Patricia Martin-Rodilla <108397432+patriciaMRodilla@users.noreply.github.com>
2025-07-29 12:38:59 +0200

    Update Experiment3_ArchaeoLegacyFuzzyMetrics.py
    Average terminology in variables

--- Experiments/Experiment3_ArchaeoLegacyFuzzyMetrics.py
+++ Experiments/Experiment3_ArchaeoLegacyFuzzyMetrics.py
@@ -1,5 +1,6 @@
 
 
+
 ###################################################################################################################################################################################
 #Instructions: The input data for replicating the experiment Illustration 3 of the article is configured in this space demarcated with #####.
 #To replicate the experiment as it appears in the paper, with all its input values, simply run the script without altering anything.
@@ -33,7 +34,7 @@ def get_mu(set, mu_funcs):
     return mu_funcs.get(set, 0)
 
 # Function for obtaining CDEG by variable
-def cal_cdeg_por_var(site, var, df, mu_funcs):
+def cal_cdeg_var(site, var, df, mu_funcs):
     cdeg_vals = []
     for index, row in df.iterrows():
         if row['site'] == site:
@@ -42,9 +43,9 @@ def cal_cdeg_por_var(site, var, df, mu_funcs):
             cdeg_vals.append(degree)
     return max(cdeg_vals)
 
-# Calls CDEG calculation by intervention
-cdeg_func_sanblas1 = cal_cdeg_por_var('SanBlas1', 'func', sites, mu_func_sanblas1)
-cdeg_func_sanblas2 = cal_cdeg_por_var('SanBlas2', 'func', sites, mu_func_sanblas2)
+# Calls CDEG calculation by archaeological intervention
+cdeg_func_sanblas1 = cal_cdeg_var('SanBlas1', 'func', sites, mu_func_sanblas1)
+cdeg_func_sanblas2 = cal_cdeg_var('SanBlas2', 'func', sites, mu_func_sanblas2)
 
 print("\nUncertainty in functionality to monitor changes in knowledge about a single site: San Blas")
 print("\nILLUSTRATION 3 EXPERIMENT. RESULTS.")
@@ -55,58 +56,58 @@ print(f"SanBlas1 - CDEG(func): {cdeg_func_sanblas1:.4f}")
 print(f"SanBlas2 - CDEG(func): {cdeg_func_sanblas2:.4f}")
 
 
-# CDEGprom calculation by intervention
-def cal_cdeg_prom_por_var(site, var, df, mu_funcs):
+# CDEGavg calculation by by archaeological intervention
+def cal_cdeg_avg_var(site, var, df, mu_funcs):
     values = df[df['site'] == site][var]
     degrees = [get_mu(value, mu_funcs) for value in values]
     if degrees:
         return sum(degrees) / len(degrees)
     return 0
 
-# Calls CDEGprom calculation by intervention
-cdeg_prom_func_sanblas1 = cal_cdeg_prom_por_var('SanBlas1', 'func', sites, mu_func_sanblas1)
-cdeg_prom_func_sanblas2 = cal_cdeg_prom_por_var('SanBlas2', 'func', sites, mu_func_sanblas2)
+# Calls CDEGavg calculation by by archaeological intervention
+cdeg_avg_func_sanblas1 = cal_cdeg_avg_var('SanBlas1', 'func', sites, mu_func_sanblas1)
+cdeg_avg_func_sanblas2 = cal_cdeg_avg_var('SanBlas2', 'func', sites, mu_func_sanblas2)
 
 print("\nPROPOSED METRICS & FUZZY FRAMEWORK FOR ARCHAEOLOGICAL LEGACY DATA")
 
-# New metrics CDEGprom and FEQ results
-print("\nCDEGprom by interventions:")
-print(f"SanBlas1 - CDEGprom(functionality): {cdeg_prom_func_sanblas1:.4f}")
-print(f"SanBlas2 - CDEGprom(functionality): {cdeg_prom_func_sanblas2:.4f}")
+# New metrics CDEGavg and FEQ results
+print("\nCDEGavg by interventions:")
+print(f"SanBlas1 - CDEGavg(functionality): {cdeg_avg_func_sanblas1:.4f}")
+print(f"SanBlas2 - CDEGavg(functionality): {cdeg_avg_func_sanblas2:.4f}")
 
 
 
-# CDEG prom (*) calculation by intervention (It's the same as CDEGprom(use) because we are only evaluationg one variable (use))
-def cal_cdeg_prom_total(df):
-    cdeg_prom_total_sanblas1 = cdeg_prom_func_sanblas1
-    cdeg_prom_total_sanblas2 = cdeg_prom_func_sanblas2
+# CDEG avg (*) calculation by by archaeological intervention (It's the same as CDEGavg(functionality) because we are only evaluationg one variable (functionality))
+def cal_cdeg_avg_total(df):
+    cdeg_avg_total_sanblas1 = cdeg_avg_func_sanblas1
+    cdeg_avg_total_sanblas2 = cdeg_avg_func_sanblas2
     # 
-    cdeg_prom_total_sanblas1 = cdeg_prom_total_sanblas1/1
-    cdeg_prom_total_sanblas2= cdeg_prom_total_sanblas2/1
-    return cdeg_prom_total_sanblas1, cdeg_prom_total_sanblas2
+    cdeg_avg_total_sanblas1 = cdeg_avg_total_sanblas1/1
+    cdeg_avg_total_sanblas2= cdeg_avg_total_sanblas2/1
+    return cdeg_avg_total_sanblas1, cdeg_avg_total_sanblas2
     
     
     
-# Call CDEG prom (*) calculation by archaeological site
-cdeg_prom_total_sanblas1, cdeg_prom_total_sanblas2=cal_cdeg_prom_total(sites)
+# Call CDEG avg (*) calculation by archaeological site
+cdeg_avg_total_sanblas1, cdeg_avg_total_sanblas2=cal_cdeg_avg_total(sites)
 
 
 # FEQ calculation by pair of archaeological sites
-def cal_feq(cdeg_prom_total_sites, site_A, site_B):
-    return cdeg_prom_total_sites[site_A] / cdeg_prom_total_sites[site_B]
+def cal_feq(cdeg_avg_total_sites, site_A, site_B):
+    return cdeg_avg_total_sites[site_A] / cdeg_avg_total_sites[site_B]
 
 # Call FEG SanBlas1/SanBlas2
-cdeg_prom_total_sites = {'SanBlas1': cdeg_prom_total_sanblas1, 'SanBlas2': cdeg_prom_total_sanblas2}
-feq_sanblas1_sanblas2 = cal_feq(cdeg_prom_total_sites, 'SanBlas1', 'SanBlas2')
+cdeg_avg_total_sites = {'SanBlas1': cdeg_avg_total_sanblas1, 'SanBlas2': cdeg_avg_total_sanblas2}
+feq_sanblas1_sanblas2 = cal_feq(cdeg_avg_total_sites, 'SanBlas1', 'SanBlas2')
 
 # Call FEG SanBlas2/SanBlas1
-cdeg_prom_total_sites = {'SanBlas2': cdeg_prom_total_sanblas2, 'SanBlas1': cdeg_prom_total_sanblas1}
-feq_sanblas2_sanblas1= cal_feq(cdeg_prom_total_sites, 'SanBlas2','SanBlas1')
+cdeg_avg_total_sites = {'SanBlas2': cdeg_avg_total_sanblas2, 'SanBlas1': cdeg_avg_total_sanblas1}
+feq_sanblas2_sanblas1= cal_feq(cdeg_avg_total_sites, 'SanBlas2','SanBlas1')
 
 
 
-print(f"SanBlas1 - CDEGprom(*): {cdeg_prom_total_sanblas1:.4f}")
-print(f"SanBlas2 - CDEGprom(*): {cdeg_prom_total_sanblas2:.4f}")
+print(f"SanBlas1 - CDEGavg(*): {cdeg_avg_total_sanblas1:.4f}")
+print(f"SanBlas2 - CDEGavg(*): {cdeg_avg_total_sanblas2:.4f}")
 
 
 print(f"\nFEQ(SanBlas1, SanBlas2): {feq_sanblas1_sanblas2:.4f}")
